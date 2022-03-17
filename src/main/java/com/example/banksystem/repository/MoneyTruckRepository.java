88606b30740de1c4a7ec5e027ad244d6e0e2e736
Thipsukhon <thipsukhon@Thipsukhons-MacBook-Pro.local>
2022-03-17 18:34:52 +0700

    #4 Edited Wrong Class in configuration

--- src/main/java/com/example/banksystem/repository/MoneyTruckRepository.java
+++ src/main/java/com/example/banksystem/repository/MoneyTruckRepository.java
@@ -1,6 +1,7 @@
 package com.example.banksystem.repository;
 
+import com.example.banksystem.entities.MoneyTruck;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MoneyTruckRepository extends JpaRepository<MoneyTruckRepository,Long> {
+public interface MoneyTruckRepository extends JpaRepository<MoneyTruck,Long> {
 }
