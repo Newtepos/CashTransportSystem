0be2bcef2e58d1eba12b4a186ee9c5d604e7b70e
newtepos <supakit.pnm@gmail.com>
2022-03-18 00:16:19 +0700

    #11: Added TruckNotFoundException Handler

--- src/main/java/com/example/banksystem/cbts/controller/CBTSControllerAdvice.java
+++ src/main/java/com/example/banksystem/cbts/controller/CBTSControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.example.banksystem.cbts.controller;
 
-import com.example.banksystem.AppResponse;
+import com.example.banksystem.cbts.exception.TruckNotFoundException;
+import com.example.banksystem.cbts.response.CBTSResponse;
 import com.example.banksystem.cbts.exception.BankNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,13 +9,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.transaction.TransactionalException;
+
 @RestControllerAdvice
 public class CBTSControllerAdvice {
 
     @ExceptionHandler(BankNotFoundException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public AppResponse bankNotFound(BankNotFoundException e) {
-        return new AppResponse(e.getMessage());
+    public CBTSResponse bankNotFound(BankNotFoundException e) {
+        return new CBTSResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(TruckNotFoundException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public CBTSResponse truckNotFound(TruckNotFoundException e) {
+        return new CBTSResponse(e.getMessage());
     }
 }
