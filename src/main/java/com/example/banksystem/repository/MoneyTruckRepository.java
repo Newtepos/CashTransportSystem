367e1605a03cde6e25e256c00ec9805e389b5b97
Thipsukhon <thipsukhon@Thipsukhons-MacBook-Pro.local>
2022-03-17 16:44:55 +0700

    #4: Created MoneyTruckRepository

---
+++ src/main/java/com/example/banksystem/repository/MoneyTruckRepository.java
@@ -0,0 +1,6 @@
+package com.example.banksystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MoneyTruckRepository extends JpaRepository<MoneyTruckRepository,Long> {
+}
