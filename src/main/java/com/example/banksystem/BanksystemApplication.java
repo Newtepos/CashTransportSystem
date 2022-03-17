5ce60dad10e58b2bd6fe67ec93e25ef976a68d24
Thipsukhon <thipsukhon@Thipsukhons-MacBook-Pro.local>
2022-03-17 18:37:34 +0700

    #5: Created InitData to DB

--- src/main/java/com/example/banksystem/BanksystemApplication.java
+++ src/main/java/com/example/banksystem/BanksystemApplication.java
@@ -1,10 +1,45 @@
 package com.example.banksystem;
 
+import com.example.banksystem.entities.*;
+import com.example.banksystem.repository.BankRepositry;
+import com.example.banksystem.repository.MoneyTruckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class BanksystemApplication {
+
+	@Autowired
+	MoneyTruckRepository moneyTruckRepository;
+
+	@Autowired
+	BankRepositry bankRepositry;
+
+	@PostConstruct
+	public void initData() {
+		Bank bank1 = new Bank("Cash Center");
+		Bank bank2 = new Bank("Branch 1");
+		bankRepositry.save(bank1);
+		bankRepositry.save(bank2);
+
+		bank1.setBankName("KBTG");
+		Cash cash = new Cash("USD", 200000.12312312);
+		CashPackage cashPackage = new CashPackage(cash, bank1, bank2);
+		MoneyTruck moneyTruck = new MoneyTruck();
+		Location location = new Location(12.12,12.12, moneyTruck);
+		Location location2 = new Location(12.12,12.12,moneyTruck);
+		moneyTruck.addPackage(cashPackage);
+		moneyTruck.addLocation(location);
+
+		moneyTruckRepository.save(moneyTruck);
+
+		moneyTruck.addLocation(location2);
+		moneyTruckRepository.save(moneyTruck);
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanksystemApplication.class, args);
